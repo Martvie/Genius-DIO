412e3ab74858efaaf6edba91ae530eeb0c05c0be
Martvie <marviedias@gmail.com>
2022-04-29 00:20:50 -0300

    game functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,91 @@
-let order, clickedOrder = []
-let score = 0
+let order = [];
+let clickedOrder = [];
+let score = 0;
 
-function colorDraw() {
-    let selected = Math.floor(Math.random() * 4)
-    order[order.length] = selected
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
 
-    for (let i in order) {
-        let elementeColor = creatElement(order[i])
-        ligthColor(elementeColor, Number(i + 1))
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+function drawColor () {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
     }
 }
 
-function ligthColor(elemente, time) {
-    let time = time * 500
+function lightColor (element, number) {
+    number = number * 500;
     setTimeout(() => {
-        elemente.classList.add("select")
-    }, time - 250 );
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    }, number);
+}
+
+function checkOrder () {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+function click (color) {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
 
     setTimeout(() => {
-        elemente.classList.remove("select")
-    }, time - 250 );
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    },250);
 }
+
+function createColorElement (color) {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+function nextLevel () {
+    score++;
+    drawColor();
+}
+
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+function playGame () {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo! 😀');
+    score = 0;
+
+    nextLevel();
+}
+
+playGame();
